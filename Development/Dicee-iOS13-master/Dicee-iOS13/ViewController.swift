0b19789f1be0bb36773da983bb341c4e4bd86adc
IOS Lab <ioslab1@iOSLab-07.local>
2024-04-13 14:00:31 -0300

    Adição de interface Dicee

--- Development/Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ Development/Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        diceImageViewOne.image =
+        [UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix")][leftDiceNumber]
+        
+        leftDiceNumber = leftDiceNumber + 1
+        
+        diceImageViewTwo.image =
+        [UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix")][rightDiceNumber]
+        
+        rightDiceNumber = rightDiceNumber - 1
+    
+    }
+    
 }
 
