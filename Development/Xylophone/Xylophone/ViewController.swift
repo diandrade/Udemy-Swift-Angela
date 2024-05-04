c6e7130ea4105bea06ce3901eaa2112394228176
iOSLab <iOSLab@iOSLab-06.local>
2024-05-04 13:57:19 -0300

    Xylophone concluído

---
+++ Development/Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,32 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        let soundName = sender.titleLabel?.text
+        sender.alpha = 0.5
+        
+        playSound(soundName: soundName!)
+        
+        print(sender.tag)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(soundName : String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
